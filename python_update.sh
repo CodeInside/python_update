c6dce1d0dbb1a65b14f1d9656939ddbc071d4b30
Alexey Zyablikov <35153440+a1xs@users.noreply.github.com>
2025-03-18 16:20:49 +0200

    python3.8-apt

--- python_update.sh
+++ python_update.sh
@@ -7,7 +7,7 @@ os_version=$(lsb_release -rs)
 if [[ "$os_version" == "18."* ]]; then
     if [[ $(echo -e "$current_version\n$required_version" | sort -V | head -n1) != "$required_version" ]]; then
         sudo apt update
-        sudo apt install -y python3.8 python3.8-dev python3.8-distutils python3.8-venv python3-pip python3-apt
+        sudo apt install -y python3.8 python3.8-dev python3.8-distutils python3.8-venv python3-pip python3.8-apt
         sudo update-alternatives --auto python3
         sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 100
     fi
